8af6dc13ca879aa4e90be5ff88ea2589f2477184
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-02-09 14:45:43 +0000

    Added layouts for the graph algorithms

---
+++ src/components/layout/GraphAlgorithmLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GraphAlgorithmLayout = () => {
+  return <div>GraphAlgorithmLayout</div>;
+};
+
+export default GraphAlgorithmLayout;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./layout.module.css";
 import SortSearchAlgorithmLayout from "./SortSearchAlgorithmLayout";
+import GraphAlgorithmLayout from "./GraphAlgorithmLayout";
+import AlgorithmContext from "../../context/algorithm-context";
 
 const Layout = (props) => {
+  const algorithmCtx = useContext(AlgorithmContext);
+
   return (
     <div className={styles.wrapper}>
-      <SortSearchAlgorithmLayout />
+      {(algorithmCtx.algorithmType === "sorting algorithms" ||
+        algorithmCtx.algorithmType === "searching algorithms" ||
+        !algorithmCtx.algorithmType) && <SortSearchAlgorithmLayout />}
+
+      {algorithmCtx.algorithmType === "graph algorithms" && (
+        <GraphAlgorithmLayout />
+      )}
     </div>
   );
 };
